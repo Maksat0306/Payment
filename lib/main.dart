0e6115540e4ccbe50ff3838d94d73b4910e7a434
kerimberdi <kerimberdi99@gmail.com>
2024-08-22 11:35:40 +0500

    provider scope

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import "package:flutter/material.dart";
+import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:toleg/screens/home_screen.dart";
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
