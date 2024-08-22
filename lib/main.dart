78f670db33c11ef7fd585a30412c1eb2dd3b0915
kerimberdi <kerimberdi99@gmail.com>
2024-08-22 18:50:42 +0500

    add themas

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import "package:flutter/material.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:toleg/screens/home_screen.dart";
+import "package:toleg/utils/text_styles.dart";
+import 'constants/colors.dart'; // AppColors sabitleri için
 
 void main() {
   runApp(
@@ -10,17 +12,26 @@ void main() {
   );
 }
 
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      theme: ThemeData(
+        scaffoldBackgroundColor: AppColors.darkBackgraund, // Genel arka plan rengi
+        appBarTheme: AppBarTheme(
+          toolbarHeight: 70,
+          backgroundColor: AppColors.darkCard,
+          titleTextStyle: AppTextStyles.appBarStyle,
+          actionsIconTheme: IconThemeData(
+            color: Colors.white,
+            size: 26,
+          ),
+        ),
+      ),
+      home: const HomeScreen(),
     );
   }
 }
-
-
